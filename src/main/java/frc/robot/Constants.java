364f0ed864269ba157bcf35c600240b8156b28a0
AHSCircuitree <113474718+AHSCircuitree@users.noreply.github.com>
2025-01-14 19:01:02 -0500

    Added some colors to Constants for LEDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,38 @@ public class Constants {
         public static final int IntakeMotorID = -1;
         public static final int AngleID = 26;
     }
+
+
+    public class Colors {
+         
+        // Fixed Patterns
+        public static double Rainbow = -.99;
+        public static double Ocean = -.95;
+        public static double Lava = -.93;
+        public static double Forest = -.91;
+
+
+        // Color Patterns
+        public static double ChaseColor = .01;
+        public static double HeartbeatColor = .05;
+        
+
+        //Solid Colors 
+        public static double HotPink = .57;
+        public static double DarkRed = .59;
+        public static double Red = .61;
+        public static double RedOrange = .63;
+        public static double Orange = .65;
+        public static double Gold = .67;
+        public static double Yellow = .69;
+        public static double LawnGreen = .71;
+        public static double Lime = .73;
+        public static double DarkGreen = .75;
+        public static double Green = .77;
+        public static double BlueGreen = .79;
+        public static double Aqua = .81;
+        
+
+
+    }
 }
