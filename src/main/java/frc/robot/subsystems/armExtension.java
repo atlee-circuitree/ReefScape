26035c85f31e7615a033f859ab99e138b5b1e9ef
Atlee Circuitree <atleecircuitree@gmail.com>
2025-01-30 17:36:33 -0500

    Lime Light In Progress

--- src/main/java/frc/robot/subsystems/armExtension.java
+++ src/main/java/frc/robot/subsystems/armExtension.java
@@ -4,6 +4,11 @@
 
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj.DigitalInput; //for limit switches
@@ -87,5 +92,56 @@ public class armExtension extends SubsystemBase {
   public void RunExtensionWithLimits(double Speed){
     extension_left.set(-Speed);
     extension_right.set(-Speed);
+  }
+
+
+  public double getDistanceToAprilTag(){
+
+    HashMap<Double, Double> AprilTags = new HashMap<>();
+    AprilTags.put(12d, 58.5d);
+    AprilTags.put(13d, 58.5d);
+    AprilTags.put(1d, 58.5d);
+    AprilTags.put(2d, 58.5d);
+    AprilTags.put(6d, 12d);
+    AprilTags.put(7d, 12d);
+    AprilTags.put(8d, 12d);
+    AprilTags.put(9d, 12d);
+    AprilTags.put(10d, 12d);
+    AprilTags.put(11d, 12d);
+    AprilTags.put(17d, 12d);
+    AprilTags.put(18d, 12d);
+    AprilTags.put(19d, 12d);
+    AprilTags.put(20d, 12d);
+    AprilTags.put(21d, 12d);
+    AprilTags.put(22d, 12d);
+
+    double ty = LimelightHelpers.getTY("cg");
+
+    double targetOffsetAngle_Vertical = ty;
+
+    double AprilTagID = /* armExtension.getAprilTagId() */ 0;
+
+    double tagHeightInches = AprilTags.get(AprilTagID);
+
+    double limelightMountAngleDegrees = 0;
+
+    double limelightHeightInches = 12;
+
+    double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+    double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+
+    double distanceFromLimelightToGoalInches = -1;
+
+    /* making a list of all april tag unique ids and depending on which april tag it sees
+     * and if list contains the april tag that the limelight sees we will set the blue or red tag heights 
+     * equal to tagHeightInches
+     */
+ 
+    distanceFromLimelightToGoalInches = (tagHeightInches - limelightHeightInches)/Math.tan(angleToGoalRadians);
+ 
+    double distanceFromLimelighttoGoalMeters = distanceFromLimelightToGoalInches / 39.37;
+ 
+    return distanceFromLimelighttoGoalMeters;
   }
 }
