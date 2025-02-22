c8c5b2bf6247001f6f46cfabb4762be8e1097f54
Atlee Circuitree <atleecircuitree@gmail.com>
2025-02-22 10:22:17 -0500

    Final armExtension pre motion magic

--- src/main/java/frc/robot/subsystems/armExtension.java
+++ src/main/java/frc/robot/subsystems/armExtension.java
@@ -38,7 +38,7 @@ public class armExtension extends SubsystemBase {
         SmartDashboard.putNumber("ArmP", 0.1);
         SmartDashboard.putNumber("ArmI", 0.0);
         SmartDashboard.putNumber("ArmD", 0.0);
-        pid = new PIDController(SmartDashboard.getNumber("armP", 0), SmartDashboard.getNumber("armI", 0), SmartDashboard.getNumber("armD", 0));
+        pid = new PIDController(SmartDashboard.getNumber("ArmP", 0.1), SmartDashboard.getNumber("ArmI", 0), SmartDashboard.getNumber("ArmD", 0));
       }
 
   public void stop() {
@@ -66,70 +66,16 @@ public class armExtension extends SubsystemBase {
     extension_right.set(-Speed);
   }
 
-
-  public double getDistanceToAprilTag(){
-
-    HashMap<Double, Double> AprilTags = new HashMap<>();
-    AprilTags.put(12d, 58.5d);
-    AprilTags.put(13d, 58.5d);
-    AprilTags.put(1d, 58.5d);
-    AprilTags.put(2d, 58.5d);
-    AprilTags.put(6d, 12d);
-    AprilTags.put(7d, 12d);
-    AprilTags.put(8d, 12d);
-    AprilTags.put(9d, 12d);
-    AprilTags.put(10d, 12d);
-    AprilTags.put(11d, 12d);
-    AprilTags.put(17d, 12d);
-    AprilTags.put(18d, 12d);
-    AprilTags.put(19d, 12d);
-    AprilTags.put(20d, 12d);
-    AprilTags.put(21d, 12d);
-    AprilTags.put(22d, 12d);
-
-    double ty = LimelightHelpers.getTY("cg");
-
-    double targetOffsetAngle_Vertical = ty;
-
-    double AprilTagID = /* armExtension.getAprilTagId() */ 0;
-
-    double tagHeightInches = AprilTags.get(AprilTagID);
-
-    double limelightMountAngleDegrees = 0;
-
-    double limelightHeightInches = 12;
-
-    double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
-    double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
-
-
-    double distanceFromLimelightToGoalInches = -1;
-
-    /* making a list of all april tag unique ids and depending on which april tag it sees
-     * and if list contains the april tag that the limelight sees we will set the blue or red tag heights 
-     * equal to tagHeightInches
-     */
- 
-    distanceFromLimelightToGoalInches = (tagHeightInches - limelightHeightInches)/Math.tan(angleToGoalRadians);
- 
-    double distanceFromLimelighttoGoalMeters = distanceFromLimelightToGoalInches / 39.37;
- 
-    return distanceFromLimelighttoGoalMeters;
-  }
-
   public void clearPID()
   {
-    pid = new PIDController(SmartDashboard.getNumber("armP", 0), SmartDashboard.getNumber("armI", 0), SmartDashboard.getNumber("armD", 0));
+    pid = new PIDController(SmartDashboard.getNumber("ArmP", 0.1), SmartDashboard.getNumber("ArmI", 0), SmartDashboard.getNumber("ArmD", 0));
     pid.reset();
   }
 
   public void runToPosition(double Distance)
   {
     pid.setSetpoint(Distance);
-    SmartDashboard.putNumber("data", pid.getSetpoint());
     double out = pid.calculate(getExtension());
-    SmartDashboard.putNumber("datat2", out);
-    SmartDashboard.putNumber("data3", getExtension());
     RunExtension(out);
   }
 }
