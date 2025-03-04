032618cb388568f9a93ab0b3bfbadec4e383bc70
Atlee Circuitree <atleecircuitree@gmail.com>
2025-03-04 16:24:02 -0500

    Changed maxangularrate

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -134,7 +134,7 @@ public class Constants {
 
     public class Drive {
         public static double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-        public static double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+        public static double MaxAngularRate = RotationsPerSecond.of(0.5).in(RadiansPerSecond); // 1/2 of a rotation per second max angular velocity
     }
 
 }
